92df4398019d5a867472ff006035c1c452f3727e
SayedTahsin <mail.tahsin99@gmail.com>
2025-04-26 22:14:01 +0600

    wip
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -5,6 +5,5 @@ const client = createClient({
   url: process.env.DATABASE_URL || "",
   authToken: process.env.DATABASE_AUTH_TOKEN,
 });
-client.execute("PRAGMA foreign_keys=ON;")
 
 export const db = drizzle({ client });
